dfbaf64cbc1510543a26cafe35b146866266abd8
Reese Robertson <reese.c.robertson@gmail.com>
2023-08-28 15:12:34 -0700

    Add format and lint definitions
    Adds format and lint scripts to hatch, using black and pylint
    respectively. Exceptions and changes as necessary to conform with them.

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,13 @@
 # list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
+# -- Pylint configuration ----------------------------------------------------
+
+# Pylint is unhappy here
+
+# pylint: disable=redefined-builtin
+
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -12,7 +19,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # import recommonmark
 # from recommonmark.transform import AutoStructify
@@ -20,9 +28,9 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'iterchain'
-copyright = '2019, Evelyn-H'
-author = 'Evelyn-H'
+project = "iterchain"
+copyright = "2019, Evelyn-H"
+author = "Evelyn-H"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,25 +39,25 @@ author = 'Evelyn-H'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
     # 'recommonmark',
-    'm2r',
+    "m2r",
 ]
 
-master_doc = 'index'
+master_doc = "index"
 
 # source_suffix = '.rst'
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -57,23 +65,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 html_sidebars = {
-    '**': [
-        'about.html',
-        'localtoc.html',
+    "**": [
+        "about.html",
+        "localtoc.html",
         # 'navigation.html',
-        'relations.html',
-        'searchbox.html',
-        'donate.html',
+        "relations.html",
+        "searchbox.html",
+        "donate.html",
     ]
 }
 
 html_theme_options = {
     # 'logo': 'logo.png',
-    'github_user': 'Evelyn-H',
-    'github_repo': 'iterchain',
+    "github_user": "Evelyn-H",
+    "github_repo": "iterchain",
     "github_banner": True,
     "github_button": True,
     "github_type": "star",
@@ -85,7 +93,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # def setup(app):
